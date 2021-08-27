0fffee6b5e21209f7320592d4d57de0fceff4e51
mariuszhermansdorfer <49192999+mariuszhermansdorfer@users.noreply.github.com>
2021-08-27 09:02:30 +0200

    Move water analysis to a separate branch

--- SandWorm/Components/SandWormComponent.cs
+++ SandWorm/Components/SandWormComponent.cs
@@ -185,8 +185,6 @@ namespace SandWorm
             colorPalettes = new List<Color>();
             DA.GetDataList(1, colorPalettes);
 
-            
-
             GenerateMeshColors(ref _vertexColors, (AnalysisTypes)_analysisType.Value, averagedDepthFrameData,
                 trimmedXYLookupTable, trimmedRGBArray,
                 _colorGradientRange.Value, (Structs.ColorPalettes)_colorPalette.Value, colorPalettes,
@@ -233,13 +231,6 @@ namespace SandWorm
                 DA.SetDataList(1, _outputWaterSurface);
                 GeneralHelpers.HideParameterGeometry(Params.Output[1]);
             }
-
-            //if (_rainDensity.Value > 0)
-            //{
-            //    RainDensity.GetGeometryForAnalysis(ref _outputGeometry, _rainDensity.Value, allPoints, trimmedWidth);
-            //    if (Params.Output.Count > 2)
-            //        DA.SetDataList(2, _outputGeometry);
-            //}
 
             DA.SetDataList(3, stats);
             ScheduleSolve();
